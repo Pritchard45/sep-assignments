ec7afc7a2b1ffeb82b03f4e304aa91f79c25168a
Pritchard45 <alexpritchard45@gmail.com>
2019-06-05 11:43:45 -0400

    stacks and queues

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -5,14 +5,22 @@ class MyQueue
   def initialize
     @queue = Array.new
     @head = @queue[0]
+    @tail = @queue.last
   end
 
   def enqueue(element)
+    @queue << element
+    @head = @queue[0]
+    @tail = @queue[-1]
   end
 
   def dequeue
+    next_up = @queue.shift
+    @head = @queue[0]
+    @tail = @queue.last
   end
 
   def empty?
+    @queue.empty?
   end
 end
